5062067fdfae163bece39a891434f7bc22dcd669
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-07-31 01:44:48 -1100

    Update wordle.cpp

--- wordle.cpp
+++ wordle.cpp
@@ -4,37 +4,68 @@
 
 class Letter_Frequency_Table{
 private:
-    std::map<char, int> Frq_Map;
+    std::map<char, std::vector<int>> Frq_Map;
+    std::string line;
+    double cnt = 0;
 
 public:
 
+    std::map<char, std::vector<int>> getMap() const { return this -> Frq_Map;};
+    double getCnt() const { return this -> cnt;}
+
     explicit Letter_Frequency_Table(std::ifstream& file){
-        std::string line;
-        int cnt = 0;
-        Frq_Map['a'];Frq_Map['b'];Frq_Map['c'];Frq_Map['d'];
-        Frq_Map['e'];Frq_Map['f'];Frq_Map['g'];Frq_Map['h'];
-        Frq_Map['i'];Frq_Map['j'];Frq_Map['k'];Frq_Map['l'];
-        Frq_Map['m'];Frq_Map['n'];Frq_Map['o'];Frq_Map['p'];
-        Frq_Map['q'];Frq_Map['r'];Frq_Map['s'];Frq_Map['t'];
-        Frq_Map['u'];Frq_Map['v'];Frq_Map['w'];Frq_Map['x'];
-        Frq_Map['y'];Frq_Map['z'];
+
+        Frq_Map['a'] = std::vector<int>(5, 0);
+        Frq_Map['b'] = std::vector<int>(5, 0);
+        Frq_Map['c'] = std::vector<int>(5, 0);
+        Frq_Map['d'] = std::vector<int>(5, 0);
+        Frq_Map['e'] = std::vector<int>(5, 0);
+        Frq_Map['f'] = std::vector<int>(5, 0);
+        Frq_Map['g'] = std::vector<int>(5, 0);
+        Frq_Map['h'] = std::vector<int>(5, 0);
+        Frq_Map['i'] = std::vector<int>(5, 0);
+        Frq_Map['j'] = std::vector<int>(5, 0);
+        Frq_Map['k'] = std::vector<int>(5, 0);
+        Frq_Map['l'] = std::vector<int>(5, 0);
+        Frq_Map['m'] = std::vector<int>(5, 0);
+        Frq_Map['n'] = std::vector<int>(5, 0);
+        Frq_Map['o'] = std::vector<int>(5, 0);
+        Frq_Map['p'] = std::vector<int>(5, 0);
+        Frq_Map['q'] = std::vector<int>(5, 0);
+        Frq_Map['r'] = std::vector<int>(5, 0);
+        Frq_Map['s'] = std::vector<int>(5, 0);
+        Frq_Map['t'] = std::vector<int>(5, 0);
+        Frq_Map['u'] = std::vector<int>(5, 0);
+        Frq_Map['v'] = std::vector<int>(5, 0);
+        Frq_Map['w'] = std::vector<int>(5, 0);
+        Frq_Map['x'] = std::vector<int>(5, 0);
+        Frq_Map['y'] = std::vector<int>(5, 0);
+        Frq_Map['z'] = std::vector<int>(5, 0);
 
         while(std::getline(file, line)){
-            for(char & i : line){
-                char lower_case_i = std::tolower(i);
-                Frq_Map[lower_case_i]++;
+            for(int i = 0; i < line.length(); i++){
+                char lower_case_i = std::tolower(line[i]);
+                Frq_Map[lower_case_i][i]++;
             }
             cnt++;
         }
+        file.clear();
+        file.seekg(0);
 
         for(auto& i : Frq_Map){
-            std::cout<< i.first <<":"<< i.second<<std::endl;
+            std::cout<< i.first <<":";
+            for(int j = 0; j < i.second.size(); j++)
+            {
+                std::cout<<i.second[j]<<" ";
+            }
+            std::cout<<std::endl;
         }
+
 
         std::cout<<cnt;
     }
-};
 
+};
 int main() {
     std::cout << "WORDLE!" << std::endl;
     std::ifstream inputFile("ro_words");
