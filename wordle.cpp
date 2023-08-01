9282c5da1b98599cd605c16e237f5cf0cc47904e
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-08-01 03:52:41 -1100

    Update wordle.cpp

--- wordle.cpp
+++ wordle.cpp
@@ -110,6 +110,71 @@ public:
 
 };
 
+
+class WordleGame{
+private:
+
+    std::vector<std::vector<char>> Board;
+    std::string RandomGuess;
+    std::string word;
+    std::vector<std::string> words;
+
+public:
+
+    void UpdateBoard(std::vector<char> guess, int index){
+        this -> Board[index] = std::move(guess);
+    }
+
+    void PrintBoard(){
+        for(auto & i : Board){
+            for(auto & j : i){
+                std::cout<<j<<" ";
+            }
+            std::cout<<std::endl;
+        }
+    }
+
+    std::vector<std::vector<char>> GetBoard() const { return this -> Board; }
+
+     explicit WordleGame(std::ifstream& file) : Board(6, std::vector<char>(5, '_')){
+         std::cout << " WORDLE" << std::endl;
+         PrintBoard();
+
+         while(std::getline(file, word)){
+             words.emplace_back(word);
+         }
+
+         std::srand(static_cast<unsigned int>(std::time(nullptr)));
+         int RandomIndex = std::rand() % words.size();
+         RandomGuess = words[RandomIndex];
+         std::vector<char> RandomGuessVector;
+         for(auto & i : RandomGuess){
+             RandomGuessVector.push_back(i);
+         }
+
+
+         std::cout<<"Todays Word: " << RandomGuess;
+
+         int cnt = 0;
+         std::vector<char> Try(5);
+         while(cnt != 6){
+             for(int i = 0; i < 5; i++) {
+                 std::cin >> Try[i];
+             }
+             UpdateBoard(Try, cnt);
+             PrintBoard();
+             if(Try == RandomGuessVector){
+                 std::cout<<"You got it in "<<cnt + 1<<" tries!"<<std::endl;
+                 break;
+             }
+             cnt++;
+
+         }
+     }
+
+
+};
+
 int main() {
 
     std::cout << "WORDLE!" << std::endl;
